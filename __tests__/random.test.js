39b90931030dcd4f00c696324f18dfc06d93806b
Moradi1412 <moradi.javid@gmail.com>
2021-12-03 00:15:48 -0800

    reviewed and add comments

--- __tests__/random.test.js
+++ __tests__/random.test.js
@@ -1,5 +1,8 @@
+//including the file needed to be tested. 
 const checkIfEqual = require ('../lib/random.js'); 
 
+//sample test files 
+//each test will have two argument, string and a function 
 test ("check if 10 is equal to 10", () => {
     expect(checkIfEqual.checkIfEqual(10,10)).toBe(true); 
 }); 
